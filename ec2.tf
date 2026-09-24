563b2342e34681cd5e79f79390af85f7d799f524
KauanCursino <kauan.cruz@hotmail.com>
2026-09-24 14:08:40 -0300

    feat: adicionando EC2 pública para frontend

--- ec2.tf
+++ ec2.tf
@@ -9,3 +9,24 @@ resource "aws_instance" "ec2_ai_server" {
 
   tags = { Name = "ec2_ai_server" }
 }
+
+resource "aws_instance" "ec2_frontend" {
+  ami                    = var.ec2_ami
+  instance_type          = var.ec2_instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ec2_frontend.id]
+  key_name               = var.ec2_key_name
+
+  tags = { Name = "ec2_frontend" }
+}
+
+resource "aws_eip" "frontend" {
+  domain = "vpc"
+
+  tags = { Name = "${var.project}-frontend-eip" }
+}
+
+resource "aws_eip_association" "frontend" {
+  instance_id   = aws_instance.ec2_frontend.id
+  allocation_id = aws_eip.frontend.id
+}
--- security_groups.tf
+++ security_groups.tf
@@ -47,3 +47,38 @@ resource "aws_security_group" "ec2_ai_server" {
 
   tags = { Name = "${var.project}-sg-ec2" }
 }
+
+resource "aws_security_group" "ec2_frontend" {
+  name   = "${var.project}-sg-frontend"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "${var.project}-sg-frontend" }
+}
